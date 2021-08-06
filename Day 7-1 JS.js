edcff7669d7f547706d8024bdcf4e508657f16fe
MichaelGameel <87283850+michaelgameel35@users.noreply.github.com>
2021-08-06 17:45:45 +0200

    Add files via upload

---
+++ Day 7-1 JS.js
@@ -0,0 +1,9 @@
+window.alert ("Welcome to our websites")
+
+document.getElementById("m1").innerHTML="WE School"
+
+document.getElementById("m2").innerHTML="Michael"
+
+function track() {document.getElementById('e3').innerHTML='Future Career Camp'}
+
+function today() {document.getElementById('e4').innerHTML = Date()}
